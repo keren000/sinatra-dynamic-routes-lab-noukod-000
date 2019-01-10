0254a6f65b0289e286caac44684be57e81ed07f8
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-10 18:18:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/reversename/:name' do
+  get "/reversename/:name" do
     params[:name].reverse
   end
 
   get '/square/:number' do
-    num = params[:number]
-    (num.to_i**2).to_s
+    square = params[:number]
+    (square.to_i**2).to_s
   end
 
   get '/say/:number/:phrase' do
